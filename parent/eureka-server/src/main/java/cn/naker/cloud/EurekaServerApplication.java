7e4f258d479420859485350ce6dc0011b807ba5a
Jeremy <1159387269@qq.com>
2021-04-10 13:10:51 +0800

    eureka-server

---
+++ parent/eureka-server/src/main/java/cn/naker/cloud/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package cn.naker.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Jeremy
+ * @date 2021/4/10
+ * @description
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
