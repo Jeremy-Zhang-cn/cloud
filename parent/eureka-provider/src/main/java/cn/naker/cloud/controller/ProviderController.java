ea34f0860455c499e06d944b5467cb96b9b3e544
Jeremy <1159387269@qq.com>
2021-04-10 13:44:35 +0800

    eureka-provider

---
+++ parent/eureka-provider/src/main/java/cn/naker/cloud/controller/ProviderController.java
@@ -0,0 +1,30 @@
+package cn.naker.cloud.controller;
+
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Jeremy
+ * @date 2021/4/10
+ * @description
+ */
+
+@RestController
+@RequestMapping(value = "/sys/provider")
+public class ProviderController {
+
+	@Resource
+	private DiscoveryClient discoveryClient;
+
+	@GetMapping(value = "/getServices")
+	public String getServices() {
+		String services = "Services: " + discoveryClient.getServices();
+		System.out.println(services);
+		return services;
+	}
+
+}
