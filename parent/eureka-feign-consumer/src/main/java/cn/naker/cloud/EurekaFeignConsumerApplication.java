82907d18b90e417d415fe9956140e405a92e3b5a
Jeremy <1159387269@qq.com>
2021-04-24 17:26:18 +0800

    移除feign加持下多余的restTemplate

--- parent/eureka-feign-consumer/src/main/java/cn/naker/cloud/EurekaFeignConsumerApplication.java
+++ parent/eureka-feign-consumer/src/main/java/cn/naker/cloud/EurekaFeignConsumerApplication.java
@@ -3,11 +3,7 @@ package cn.naker.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Zhang Dingjie
@@ -22,12 +18,6 @@ public class EurekaFeignConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaFeignConsumerApplication.class, args);
-	}
-
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
 	}
 
 }
--- parent/eureka-hystrix-consumer/src/main/java/cn/naker/cloud/EurekaHystrixConsumerApplication.java
+++ parent/eureka-hystrix-consumer/src/main/java/cn/naker/cloud/EurekaHystrixConsumerApplication.java
@@ -4,11 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Zhang Dingjie
@@ -25,13 +21,6 @@ public class EurekaHystrixConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaHystrixConsumerApplication.class, args);
-	}
-
-
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
 	}
 
 }
