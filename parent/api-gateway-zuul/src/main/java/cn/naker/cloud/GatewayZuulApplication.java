e891c83891ddbb8cc92080d49f30f18ec4befdfb
JeremyZhang <1159387269@qq.com>
2021-04-25 11:15:38 +0800

    Zuul网关

---
+++ parent/api-gateway-zuul/src/main/java/cn/naker/cloud/GatewayZuulApplication.java
@@ -0,0 +1,20 @@
+package cn.naker.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+// 开启Zuul网关代理
+@EnableZuulProxy
+public class GatewayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayZuulApplication.class, args);
+    }
+
+}
