2149a77398b862b3a4b731be43d1d3cef58cb893
JeremyZhang <1159387269@qq.com>
2022-08-16 16:25:16 +0800

    @Order

--- parent/canal/src/main/java/cn/naker/cloud/CanalRunner.java
+++ parent/canal/src/main/java/cn/naker/cloud/CanalRunner.java
@@ -2,6 +2,7 @@ package cn.naker.cloud;
 
 import cn.naker.cloud.client.CanalClient;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.Executors;
@@ -12,9 +13,10 @@ import java.util.concurrent.TimeUnit;
 /**
  * @author Zhang Dingjie
  * @date 2022/8/16
- * @Description run方法只会在容器启动时执行一次，并且如果实现了CommandLineRunner接口的bean执行run方法出错 容器会停掉
+ * @Description run方法只会在容器启动时执行一次，并且如果实现了CommandLineRunner接口的bean执行run方法出错 容器会停掉 @Order注解指定执行顺序
  */
 @Component
+@Order(value = 1)
 public class CanalRunner implements CommandLineRunner {
 
 	@Override
